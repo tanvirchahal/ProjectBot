9c19980d4894c1a326fa3cd97fb29a8345b023f9
Tanvir Chahal <tnvr009@gmail.com>
2016-08-01 15:59:21 +0530

    Authentication Success to App Version 1

--- cURL/connect.php
+++ cURL/connect.php
@@ -3,9 +3,12 @@
      * Developed by Tanvir Chahal
      * 01-Aug-2016
      */
-     $ch = curl_init("https://api.wit.ai/message");
+     $ch = curl_init();
+     $header = array("Authorization: Bearer YPLIY7UOHEJSJSMQMJBWIUIEGFQODIJ2");
      
-     curl_setopt($ch, CURLOPT_HEADER, "Authorization: Bearer YPLIY7UOHEJSJSMQMJBWIUIEGFQODIJ2");
+     curl_setopt($ch, CURLOPT_URL, "https://api.wit.ai/message?v=20160801&q=Hello");
+     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+     curl_setopt($ch, CURLOPT_POST, true);
      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
      
